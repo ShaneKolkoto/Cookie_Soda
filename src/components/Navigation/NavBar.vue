4a9d00b97f025a424a0cb71f81128a1bc3179151
ShaneKolkoto <shanekolkoto@gmail.com>
2022-08-31 20:58:35 +0200

    fixed navigation and added store

--- src/components/Navigation/NavBar.vue
+++ src/components/Navigation/NavBar.vue
@@ -1,39 +1,50 @@
 <template>
-  <v-app-bar app color="white" flat>
+  <v-app-bar app color="white" flat hide-on-scroll>
     <v-badge color="#D5F0DB" dot>
       <v-img src="logo.png" contain width="40"></v-img>
     </v-badge>
-
-    <v-toolbar-title class="ml-4">
+    <v-toolbar-title class="ml-2">
       <strong>Cookie</strong><span class="green--text">&</span
       ><strong>Soda</strong>
     </v-toolbar-title>
 
     <v-spacer></v-spacer>
-    <v-badge color="#FF6D59" overlap content="2" class="mr-2 mt-1">
-      <v-avatar color="#FFF0EE" size="40">
-        <v-icon x-small color="#FF6D59">far fa-heart</v-icon>
-      </v-avatar>
+    <v-avatar size="40" class="mr-2 mt-1" v-if="GET_USER">
+      <img src="https://cdn.vuetifyjs.com/images/john.jpg" alt="avatar" />
+    </v-avatar>
+    <v-badge
+      bordered
+      color="error"
+      icon="mdi-lock"
+      overlap
+      class="mr-2 mt-1"
+      v-if="!GET_USER"
+    >
+      <v-btn class="white--text" color="error" depressed>Login</v-btn>
     </v-badge>
-    <v-badge color="#41AB55" overlap content="3" class="mr-2 mt-1">
-      <v-avatar color="#ECF7EE" size="40">
-        <v-icon x-small color="#41AB55" @click="drawer = true"
-          >fas fa-shopping-cart</v-icon
-        >
-      </v-avatar>
+
+    <v-badge
+      bordered
+      color="success"
+      icon="mdi-lock-open"
+      overlap
+      class="mr-2 mt-1"
+      v-else
+    >
+      <v-btn class="white--text" color="success" depressed>Logout</v-btn>
     </v-badge>
   </v-app-bar>
 </template>
 
 <script>
+import { mapGetters } from "vuex";
 export default {
   data() {
-    return {
-      drawer: false,
-    };
+    return {};
   },
-  components: {},
-  methods: {},
+  computed: {
+    ...mapGetters("user", ["GET_USER"]),
+  },
 };
 </script>
 
