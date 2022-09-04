bca0fe5933c02baed2b15b9e4494a1d3cec6131e
ShaneKolkoto <shanekolkoto@gmail.com>
2022-09-04 21:45:53 +0200

    fixed production

--- knexfile.js
+++ knexfile.js
@@ -48,7 +48,7 @@ module.exports = {
 
   production: {
     client: "pg",
-    connection: process.env.DATABASE_URL,
+    connectionString: process.env.DATABASE_URL,
     migrations: {
       directory: "./db/migrations",
     },
@@ -56,7 +56,6 @@ module.exports = {
       directory: "./db/seeds",
     },
     ssl: {
-      require: true,
       rejectUnauthorized: false,
     },
   },
