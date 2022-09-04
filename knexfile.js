1f9ad7f2d486b51b7dd3b9719d60f7721746245b
ShaneKolkoto <shanekolkoto@gmail.com>
2022-09-04 21:50:29 +0200

    fixed production

--- knexfile.js
+++ knexfile.js
@@ -48,7 +48,7 @@ module.exports = {
 
   production: {
     client: "pg",
-    connectionString: process.env.DATABASE_URL,
+    connection: process.env.DATABASE_URL,
     migrations: {
       directory: "./db/migrations",
     },
