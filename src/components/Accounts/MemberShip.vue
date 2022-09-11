806185ae1b388a69d85a8f2ad01949d1f52c972b
ShaneKolkoto <shanekolkoto@gmail.com>
2022-09-11 12:38:58 +0200

    revamp added shop page

---
+++ src/components/Accounts/MemberShip.vue
@@ -0,0 +1,36 @@
+<template>
+  <!-- <v-container app fluid> -->
+  <v-btn width="100%" class="ma-1" plain>
+    <v-row style="background-color: purple" class="mt-2 mb-2">
+      <v-col cols="12">
+        <v-row align="center" justify="center">
+          <v-col cols="12" class="text-center text-uppercase">
+            <div class="white--text">Become A Member</div>
+          </v-col>
+        </v-row>
+      </v-col>
+    </v-row>
+  </v-btn>
+  <!-- <v-col cols="3"> -->
+  <!-- <v-icon x-large style="color: white">mdi-account-circle</v-icon>
+    </v-col>
+
+    <v-col cols="5">
+      <p class="text-uppercase subtitle-1">Manage your account</p>
+    </v-col>
+
+    <v-col cols="4">
+      <v-btn>View Now</v-btn>
+    </v-col> -->
+  <!-- </v-container> -->
+</template>
+
+<script>
+export default {
+  name: "account-banner",
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
